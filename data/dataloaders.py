e5efa36b248bd3f3a806b7848904b82d582480da
vincent-doan <vinh.dt210940@sis.hust.edu.vn>
2023-11-20 13:54:48 +0700

    added param for output size

--- data/dataloaders.py
+++ data/dataloaders.py
@@ -15,25 +15,29 @@ cache_dir = os.path.join(current_dir, "cache")
 os.makedirs(cache_dir, exist_ok=True)
 
 class DIV2KDataset(Dataset):
-    def __init__(self, split:str="train", downscale_type:str="bicubic", scale:int=2, transforms:Compose=None, seed:int=1989, cache_dir:str=cache_dir):
+    def __init__(self, split:str="train", downscale_type:str="bicubic", scale:int=2, transforms:Compose=None, seed:int=1989, output_size:tuple=(384, 384), 
+                 cache_dir:str=cache_dir):
+        
         self.dataset = load_dataset("eugenesiow/Div2k", "{}_x{}".format(downscale_type, scale), split=split, cache_dir=cache_dir)
         self.scale = scale
         self.transforms = transforms
         self.seed = seed
+        self.output_size = output_size
     
     def __getitem__(self, index:int):
         lr_path = self.dataset[index]["lr"]
         hr_path = self.dataset[index]["hr"]
         lr_image = Image.open(lr_path)
         hr_image = Image.open(hr_path)
-        
+
+        w, h = hr_image.size
+        th, tw = self.output_size
         # Random crop
-        while True:
-            i, j, h, w = transforms.RandomCrop.get_params(hr_image, output_size=(768, 768))
-            if i % self.scale == 0 and j % self.scale == 0:
-                break
-        hr_image = transforms.functional.crop(hr_image, i, j, h, w)
-        lr_image = transforms.functional.crop(lr_image, i//self.scale, j//self.scale, h//self.scale, w//self.scale)
+        i = random.choice(list(range(0, h-th+1, self.scale)))
+        j = random.choice(list(range(0, w-tw+1, self.scale)))
+        
+        hr_image = transforms.functional.crop(hr_image, i, j, th, tw)
+        lr_image = transforms.functional.crop(lr_image, i//self.scale, j//self.scale, th//self.scale, tw//self.scale)
 
         if self.transforms:
             torch.manual_seed(seed=self.seed)
@@ -51,7 +55,9 @@ class DIV2KDataset(Dataset):
         return len(self.dataset)
     
 class DIV2KDataLoader(DataLoader):
-    def __init__(self, split:str="train", downscale_type:str="bicubic", scale:int=4, transforms:Compose=None, seed:int=1989, cache_dir:str=cache_dir, batch_size:int=1, shuffle:bool=True, num_crops:int=1):
-        sub_datasets = [DIV2KDataset(split=split, downscale_type=downscale_type, scale=scale, transforms=transforms, seed=seed+int(random.random()*10), cache_dir=cache_dir) for _ in range(num_crops)]
+    def __init__(self, split:str="train", downscale_type:str="bicubic", scale:int=4, transforms:Compose=None, seed:int=1989, output_size:tuple=(384, 384), 
+                 cache_dir:str=cache_dir, batch_size:int=1, shuffle:bool=True, num_crops:int=1):
+        
+        sub_datasets = [DIV2KDataset(split=split, downscale_type=downscale_type, scale=scale, transforms=transforms, seed=seed+int(random.random()*10), output_size=output_size, cache_dir=cache_dir) for _ in range(num_crops)]
         self.dataset = ConcatDataset(sub_datasets)
         super().__init__(self.dataset, batch_size=batch_size, shuffle=shuffle)
